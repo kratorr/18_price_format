4fcaf734f7e6171904846e93e504a4f0042340b7
vladimir <smirnovv91@gmail.com>
2018-03-29 20:38:09 +0300

    fixed format func

--- format_price.py
+++ format_price.py
@@ -1,20 +1,15 @@
 import sys
-import math
 
 
 def format_price(price):
-    min_fraction = 0.01
     try:
         price = round(float(price), 2)
-        fraction, _ = math.modf(price)
-        if fraction < min_fraction:
-            return "{:,.0f}".format(price).replace(',', ' ')
-        elif str(price).endswith("0"):
-            return "{:,.1f}".format(price).replace(',', ' ')
-        else:
-            return "{:,.2f}".format(price).replace(',', ' ')
     except ValueError:
         return None
+    if (price).is_integer():
+        return "{:,.0f}".format(price).replace(',', ' ')
+    else:
+        return "{:,.2f}".format(price).replace(',', ' ')
 
 
 if __name__ == "__main__":
